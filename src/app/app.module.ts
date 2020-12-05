04064b3f022be5772e7bfbcc48363cb6f77f307e
Robert <robertjwayne@gmail.com>
2020-12-05 13:44:25 +0700

    Move store devtools module into separate file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import { modules } from './modules';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    modules,
   ],
   providers: [],
   bootstrap: [AppComponent]
