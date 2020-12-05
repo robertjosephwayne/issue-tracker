bacb0c4b0e53b21f7e3923a85f6f617c1f1e87ca
Robert <robertjwayne@gmail.com>
2020-12-05 15:59:13 +0700

    Add NewIssueComponent and ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/index';
 import { modules } from './modules';
+
+import { AppComponent } from './app.component';
 import { IssuesComponent } from './components/issues/issues.component';
+import { NewIssueComponent } from './components/new-issue/new-issue/new-issue.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssuesComponent
+    IssuesComponent,
+    NewIssueComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     StoreModule.forRoot(reducers),
     modules,
   ],
