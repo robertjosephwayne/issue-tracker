86b9bf7301c3c16a647ef91230dbd2b88b4d0370
Robert <robertjwayne@gmail.com>
2020-12-05 15:16:28 +0700

    Add Issue, Filter, IssueState, Issues interfaces

--- src/app/store/issue/issue.state.ts
+++ src/app/store/issue/issue.state.ts
@@ -1,3 +1,27 @@
-export interface IssueState { }
+export interface Issue {
+  id: string;
+  title: string;
+  description: string;
+  priority: "low" | "medium" | "high";
+  resolved: boolean;
+}
 
-export const initialState: IssueState = {};
+export interface Filter {
+  text: string;
+}
+
+export interface IssueState {
+  entities: Issues;
+  filter: Filter;
+}
+
+export interface Issues {
+  [id: string]: Issue;
+}
+
+export const initialState: IssueState = {
+  entities: {},
+  filter: {
+    text: ""
+  },
+};
