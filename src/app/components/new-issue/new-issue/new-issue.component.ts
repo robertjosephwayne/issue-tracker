a7b1b6b68e497db47061ad1eea822b1e7f68d6a6
Robert <robertjwayne@gmail.com>
2020-12-05 15:58:29 +0700

    Create new issue form

--- src/app/components/new-issue/new-issue/new-issue.component.ts
+++ src/app/components/new-issue/new-issue/new-issue.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import * as IssueActions from '../../../store/issue/issue.actions';
+
+import { RootState } from 'src/app/store';
 
 @Component({
   selector: 'app-new-issue',
@@ -6,8 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-issue.component.scss']
 })
 export class NewIssueComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private store: Store<RootState>,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      title: ["", Validators.required],
+      description: ["", Validators.required],
+      priority: ["low", Validators.required],
+    });
+  }
+
+  submit(): void {
+    const issue = this.form.value;
+    this.store.dispatch(IssueActions.submit(issue));
+  }
 
   ngOnInit(): void {
   }
